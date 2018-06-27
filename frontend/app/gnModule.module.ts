e29c9bc91af5de3f5d30441165df32cbeffb37df
vincent <vincent_pilorge@natural-solutions.eu>
2018-06-27 10:59:04 +0200

    Fix bug NgbModal

--- frontend/app/gnModule.module.ts
+++ frontend/app/gnModule.module.ts
@@ -6,6 +6,10 @@ import { GN2CommonModule } from "@geonature_common/GN2Common.module";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { ExportsListComponent, NgPBar } from "./exports-list/exports-list.component";
 import { ExportService } from "./services/export.service";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { NgbModalStack } from "@ng-bootstrap/ng-bootstrap/modal/modal-stack";
+import { NgbModalWindow } from "@ng-bootstrap/ng-bootstrap/modal/modal-window";
+import { NgbModalBackdrop } from "@ng-bootstrap/ng-bootstrap/modal/modal-backdrop";
 
 const routes: Routes = [
   { path: "", component: ExportsListComponent }
@@ -20,11 +24,21 @@ const routes: Routes = [
   ],
   declarations: [
     ExportsListComponent,
+    NgbModalBackdrop,
+    NgbModalWindow,
     NgPBar
   ],
   providers: [
-    ExportService
+    ExportService, 
+    NgbModal,
+    // NgbModalWindow,
+    NgbModalStack
+  ],
+  entryComponents: [
+    NgbModalBackdrop,
+    NgbModalWindow
   ],
   bootstrap: []
 })
+
 export class GeonatureModule {}
