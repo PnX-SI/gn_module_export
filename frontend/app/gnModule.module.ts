47e8a68ef91faa25f9e6543ca6eddaded2e7a043
vincent <vincent_pilorge@natural-solutions.eu>
2018-07-05 10:49:14 +0200

    Update API & fix modal bug

--- frontend/app/gnModule.module.ts
+++ frontend/app/gnModule.module.ts
@@ -6,10 +6,6 @@ import { GN2CommonModule } from "@geonature_common/GN2Common.module";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { ExportsListComponent, NgPBar } from "./exports-list/exports-list.component";
 import { ExportService } from "./services/export.service";
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
-import { NgbModalStack } from "@ng-bootstrap/ng-bootstrap/modal/modal-stack";
-import { NgbModalWindow } from "@ng-bootstrap/ng-bootstrap/modal/modal-window";
-import { NgbModalBackdrop } from "@ng-bootstrap/ng-bootstrap/modal/modal-backdrop";
 
 const routes: Routes = [
   { path: "", component: ExportsListComponent }
@@ -24,15 +20,10 @@ const routes: Routes = [
   ],
   declarations: [
     ExportsListComponent,
-    // NgbModalBackdrop,
-    // NgbModalWindow,
     NgPBar
   ],
   providers: [
     ExportService, 
-    // NgbModal,
-    // NgbModalWindow,
-    // NgbModalStack
   ],
   bootstrap: []
 })
