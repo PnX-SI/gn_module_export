6e0d3e1acafd413441166f4e2be1d293ae94ede6
patkap <patkap@users.noreply.github.com>
2018-07-13 08:18:36 +0200

    [wip] export create_or_update, delete.

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 import logging
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
 from flask import (
     Blueprint,
     session,
@@ -26,7 +27,7 @@ from .models import (
     format_map_mime)
 
 
-logger = logging.getLogger(__name__)
+logger = current_app.logger
 logger.setLevel(logging.DEBUG)
 
 EXPORTS_FOLDER = os.path.join(current_app.static_folder, 'exports')
@@ -42,35 +43,79 @@ except Exception as e:
 blueprint = Blueprint('exports', __name__)
 
 
-# garder GET durant le dev/test BE ss auth
-@blueprint.route('/create', methods=['GET', 'POST', 'PUT'])
+# DOING NEXT: mv GET -> export format selection -> download
+@blueprint.route('/export', defaults={'id_export': None}, methods=['GET', 'POST'])   # noqa E501
+@blueprint.route('/export/<id_export>', methods=['GET', 'POST', 'PUT'])
 # @fnauth.check_auth_cruved('C', True, id_app=ID_MODULE)
 # @fnauth.check_auth(3, True)
 @json_resp
-def create(info_role=None):
-    # FIXME: auth
-    # logger.debug('info_role', info_role)
+# def create_or_update_export(info_role=None, id_export=None):
+def create_or_update_export(id_export=None):
+    # logger.debug(info_role)
+    info_role = None
     id_role = info_role.id_role if info_role else 1
 
-    payload = request.get_json()
-    label = payload.get('label', None)
-    selection = payload.get('selection', None)
+    # payload = request.get_json()
+    # payload.get('label', None)
+    # payload.get('selection', None)  # noqa E501
+    if not id_export:
+        id_export = request.args.get('id_export', None)
+        id_export = int(id_export)
+
+    label = request.args.get('label', None)
+    selection = request.args.get('selection', None)
 
     if label and selection:
+        if not id_export:
+            try:
+                export = Export(id_role, label, selection)
+                DB.session.add(export)
+                DB.session.commit()
+                return export.as_dict()
+            except IntegrityError as e:
+                DB.session.rollback()
+                logger.warn('%s', str(e))
+                if '(label)=({})'.format(label) in str(e):
+                    return {
+                        'error': 'Label {} is already registered.'.format(label)
+                        }, 400
+                else:
+                    raise
+        else:
+            try:
+                export = DB.session.query(Export)\
+                                   .filter(Export.id == id_export)\
+                                   .one()
+                export.label = label if label else export.label
+                export.selection = selection if selection else export.selection
+                DB.session.commit()
+                return export.as_dict(), 200
+            except Exception as e:
+                DB.session.rollback()
+                logger.warn(str(e))
+                return {'error': 'Echec mise à jour.'}, 500
+    else:
+        return {
+            'error': 'Missing {} parameter.'. format(
+                'label' if selection else 'selection')}, 400
+
+
+@blueprint.route('/export/<id_export>', methods=['DELETE'])
+@json_resp
+def delete_export(id_export):
+    try:
+        export = Export.query.filter_by(id_export=id_export).one()
+    except NoResultFound:
+        return {'error': 'No result.'}, 404
+    else:
         try:
-            export = Export(id_role, label, selection)
-            DB.session.add(export)
+            DB.session.delete(export)
             DB.session.commit()
-            return export.as_dict()
-        except (IntegrityError) as e:
+            return {'result': 'success'}, 204
+        except Exception as e:
             DB.session.rollback()
             logger.warn('%s', str(e))
-            return {'status': 400,
-                    'error': 'Label {} is already registered.'.format(label)}
-
-    else:
-        return {'status': 400,
-                'error': 'Missing {} parameter.'. format('label' if selection else 'selection')}  # noqa E501
+            return {'error': 'Echec de suppression.'}
 
 
 @blueprint.route('/')
@@ -85,10 +130,12 @@ def create(info_role=None):
 #     logger.debug('cruved_user', user_cruved)
 @json_resp
 def getExports(info_role=1):
+
     exports = Export.query\
                     .filter(Export.status >= 0)\
                     .group_by(Export.label, Export.id)\
                     .order_by(Export.start.desc())\
                     .limit(50)\
                     .all()
-    return [export.as_dict() for export in exports] or {'error': 'No available export.'}, 204  # noqa E501
+    return [export.as_dict() for export in exports] or {
+        'error': 'No available export.'}, 204
