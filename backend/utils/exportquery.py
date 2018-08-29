a7a8af4262e6a47950400bae2ab01fe6ed6cd213
patkap <patkap@users.noreply.github.com>
2018-08-29 12:10:08 +0200

    Incorporate query filters builder.

--- backend/utils/exportquery.py
+++ backend/utils/exportquery.py
@@ -3,7 +3,7 @@ from flask import current_app
 # from geojson import FeatureCollection
 from geonature.utils.utilssqlalchemy import GenericQuery
 
-# from .exportfilter import CompositeFilter
+from .exportfilter import CompositeFilter
 
 
 logger = current_app.logger
@@ -11,20 +11,28 @@ logger.setLevel(logging.DEBUG)
 
 
 # TODO: override original build_query_filters()
+# TODO: Timing data extraction
 class ExportQuery(GenericQuery):
     def __init__(
             self,
             id_role,
             db_session,
-            tableName, schemaName, geometry_field,
-            filters, limit, offset=0):
+            table_name,
+            schema_name,
+            geometry_field,
+            filters,
+            limit, offset=0):
         self.user = id_role
 
         super().__init__(
             db_session,
-            tableName, schemaName, geometry_field,
+            table_name, schema_name, geometry_field,
             filters, limit, offset)
 
         logger.debug('User perm: %s', self.user)
 
-    # def build_query_filters():
+    def build_query_filters(self, query, filters):
+        return CompositeFilter.apply(None, query, filters=filters)
+
+    def build_query_filter(self, query, param_name, param_value):
+        return query
