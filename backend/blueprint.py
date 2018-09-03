b8c794941279d88a92c5b4e8cc8d754ba3ba29d7
patkap <patkap@users.noreply.github.com>
2018-09-03 11:13:50 +0200

    Merge branch 'primer' of https://github.com/PnX-SI/gn_module_export into primer

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -224,7 +224,7 @@ def test_view():
     from geonature.utils.env import DB
     from geonature.core.gn_synthese.models import Synthese
 
-    from .utils.view import View
+    from .utils.views import View
 
     selectable = sqlalchemy.sql.expression.select([
         Synthese.id_synthese,
