238de6b9ae41d91a1fb0420fc3ed0678d06226fa
patkap <patkap@users.noreply.github.com>
2018-09-04 09:16:37 +0200

    Add and comment some doc refs for our view creation strategy.

--- backend/utils/views.py
+++ backend/utils/views.py
@@ -1,3 +1,4 @@
+# inspiration: https://bitbucket.org/zzzeek/sqlalchemy/wiki/UsageRecipes/Views
 from sqlalchemy.schema import DDLElement
 from sqlalchemy.ext.compiler import compiles
 from flask import current_app
@@ -15,7 +16,8 @@ class CreateView(DDLElement):
 
 @compiles(CreateView)
 def visit_create_view(element, compiler, **kw):
-    # return "CREATE VIEW gn_exports.%s AS (%s)" % (
+    # DOING: fix pg schema name in generated sql
+    # raise
     return "CREATE OR REPLACE VIEW gn_exports.%s AS (%s)" % (
         element.name,
         compiler.sql_compiler.process(element.selectable))
@@ -33,7 +35,12 @@ def visit_drop_view(element, compiler, **kw):
 
 def View(name, metadata, selectable):
     t = DB.table(name)
+    # https://docs.sqlalchemy.org/en/latest/core/selectable.html?highlight=table#sqlalchemy.sql.expression.TableClause  # noqa: E501
+    # table(name, *columns) return is an instance of TableClause,
+    # which represents the “syntactical” portion of
+    # the schema-level Table object.
     for c in selectable.c:
+        # https://bitbucket.org/zzzeek/sqlalchemy/src/081d4275cf5c3e6842c8e0198542ff89617eaa96/lib/sqlalchemy/sql/elements.py?at=master&fileviewer=file-view-default#elements.py-744  # noqa: E501
         c._make_proxy(t)
     CreateView(name, selectable).execute_at('after-create', metadata)
     DropView(name).execute_at('before-drop', metadata)
