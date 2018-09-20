a6f48dd4fcd50a03878d1e517e34212c02fe7422
patkap <patkap@users.noreply.github.com>
2018-09-21 01:53:37 +0200

    mv event.ok check upstream

--- frontend/app/services/export.service.ts
+++ frontend/app/services/export.service.ts
@@ -102,28 +102,28 @@ export class ExportService {
     })
     let subscription = source.subscribe(
       event => {
-        switch(event.type) {
-          case(HttpEventType.DownloadProgress):
-            if (event.hasOwnProperty('total')) {
-              const percentage = Math.round((100 / event.total) * event.loaded)
-              this.downloadProgress.next(percentage)
-            } else {
-              const kb = (event.loaded / 1024).toFixed(2)
-              this.downloadProgress.next(parseFloat(kb))
-            }
-            break
+        if (event.ok) {
+          switch(event.type) {
+            case(HttpEventType.DownloadProgress):
+              if (event.hasOwnProperty('total')) {
+                const percentage = Math.round((100 / event.total) * event.loaded)
+                this.downloadProgress.next(percentage)
+              } else {
+                const kb = (event.loaded / 1024).toFixed(2)
+                this.downloadProgress.next(parseFloat(kb))
+              }
+              break
 
-          case(HttpEventType.ResponseHeader):
-            if (event.ok) {
+            case(HttpEventType.ResponseHeader):
               const disposition = event.headers.get('Content-Disposition')
               const match = disposition ? /filename="?([^"]*)"?;?/g.exec(disposition) : undefined;
               fileName = match && match.length > 1 ? match[1] : undefined;
-            }
-            break
+              break
 
-          case(HttpEventType.Response):
-            this._blob = new Blob([event.body], {type: event.headers.get('Content-Type')})
-            break
+            case(HttpEventType.Response):
+              this._blob = new Blob([event.body], {type: event.headers.get('Content-Type')})
+              break
+          }
         }
     },
     (response: ApiErrorResponse) => {
