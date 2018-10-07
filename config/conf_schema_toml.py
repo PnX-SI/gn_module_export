08189e5ad2eed55621f27624723b104d621d7188
patkap <patkap@users.noreply.github.com>
2018-10-07 23:01:42 +0200

    add shapefile to config schema

---
+++ config/conf_schema_toml.py
@@ -0,0 +1,26 @@
+'''
+   Spécification du schéma toml des paramètres de configurations
+'''
+
+from marshmallow import fields
+from geonature.utils.config_schema import GnModuleProdConf
+
+
+export_format_map = {
+    'csv': {
+        'mime': 'text/csv'
+        },
+    'json': {
+        'mime': 'application/json'
+        },
+    'shp': {
+        'mime': 'application/zip'
+        },
+    'rdf': {
+        'mime': 'application/rdf+xml'
+        }
+}  # noqa: E133
+
+
+class GnModuleSchemaConf(GnModuleProdConf):
+    export_format_map = fields.Dict(missing=export_format_map)
