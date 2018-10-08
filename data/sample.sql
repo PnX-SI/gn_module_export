eb93403a07f73ec28b15ab28f749e8fe39c8d3cf
pat <patkap@users.noreply.github.com>
2018-10-08 14:22:28 +0200

    fix #19 option 1

--- data/sample.sql
+++ data/sample.sql
@@ -8,7 +8,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
-INSERT INTO gn_exports.t_exports (id, label, schema_name, view_name, "desc", geometry_field, geometry_srid) VALUES (1, 'OccTax - DLB', 'pr_occtax', 'export_occtax_dlb', 'Dépôt Légal de Biodiversité', 'geom_4326', 4326);
-INSERT INTO gn_exports.t_exports (id, label, schema_name, view_name, "desc", geometry_field, geometry_srid) VALUES (2, 'OccTax - SINP', 'pr_occtax', 'export_occtax_sinp', 'SINP compliant dataset', NULL, NULL);
+INSERT INTO gn_exports.t_exports (label, schema_name, view_name, "desc", geometry_field, geometry_srid) VALUES ('OccTax - DLB', 'pr_occtax', 'export_occtax_dlb', 'Dépôt Légal de Biodiversité', 'geom_4326', 4326);
+INSERT INTO gn_exports.t_exports (label, schema_name, view_name, "desc", geometry_field, geometry_srid) VALUES ('OccTax - SINP', 'pr_occtax', 'export_occtax_sinp', 'SINP compliant dataset', NULL, NULL);
 
 SELECT pg_catalog.setval('gn_exports.t_exports_id_seq', 1, false);
