e41f1a64808b9ad0b37293f5a940be8136e9bf75
pat <patkap@users.noreply.github.com>
2018-11-22 18:41:27 +0100

    testing apprenticeship in progress

--- backend/tests/api_test.py
+++ backend/tests/api_test.py
@@ -13,11 +13,12 @@ from flask import (
 
 sys.path.append(os.path.expanduser('~/geonature/backend/tests'))
 from .bootstrap_test import (
-    # app,
+    app,  # pylint: disable=W0611
     # post_json,
     # json_of_response,
     get_token
 )  # noqa: E402
+assert app  # silence pyflake's unused import warning by using assert as a “usage” of the empty import.  # noqa: E501
 # UserWarning: The psycopg2 wheel package will be renamed from release 2.8 ...
 import warnings  # noqa: E402
 warnings.filterwarnings("ignore", category=UserWarning, module='psycopg2')
@@ -38,8 +39,17 @@ class TestApiModuleExports:
     def test_getExports(self):
         token = get_token(self.client)
         self.client.set_cookie('/', 'token', token)
-        response = self.client.get(
-            url_for('exports.getExports')
-        )
+        response = self.client.get(url_for('exports.getExports'))
+        print('data:', response.data)
+        assert response.status_code == 200
 
+    def test_etalab(self):
+        response = self.client.get(url_for('exports.etalab_export'))
+        assert response.status_code == 200
+
+    def test_export(self):
+        token = get_token(self.client)
+        self.client.set_cookie('/', 'token', token)
+        response = self.client.get(
+            url_for('exports.export', id_export=1, format='csv'))
         assert response.status_code == 200
---
+++ backend/tests/delete_sample_data.sql
@@ -0,0 +1,3 @@
+TRUNCATE gn_exports.t_exports CASCADE;
+
+-- DELETE FROM utilisateurs.cor_app_privileges WHERE id_role = 3;
