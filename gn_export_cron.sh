e2d2cbb11e9db3e816f4ac10a109945c73c1347d
pat <patkap@users.noreply.github.com>
2018-12-08 11:09:16 +0100

    cron job: re enable ID_APPLICATION parsing. No need to save the data twice.

--- gn_export_cron.sh
+++ gn_export_cron.sh
@@ -23,11 +23,9 @@ fetch_export() {
     check_request_status $status
 }
 
-X_FILE="${X_FILE:-${HOME}/geonature/backend/static/exports/export_etalab.ttl}"
+X_FILE="${X_FILE:-/dev/null}"
 
-# TODO: request http://instance_name/gn_commons/modules and get module_path but no ID_APPLICATION
-#       alternative:
-ID_APPLICATION=1  # FIXME: $(grep ID_APPLICATION ~/geonature/config/geonature_config.toml | cut -f3 -d ' ')
+ID_APPLICATION=$(grep ID_APPLICATION ~/geonature/config/geonature_config.toml | cut -f3 -d ' ')
 API_ENDPOINT=$(grep API_ENDPOINT ~/geonature/config/geonature_config.toml | cut -f3 -d ' ' | sed -e "s/'//g")
 API_URL=$(grep api_url ~/geonature/external_modules/exports/config/conf_gn_module.toml | cut -f3 -d ' ' | sed -e "s/'//g")
 
