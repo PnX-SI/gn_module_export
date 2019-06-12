9a29c6eb9175f0799b5402da7670cb4f8bbdd34f
amandine.sahl <amandine.sahl@gmail.com>
2019-06-12 17:54:57 +0200

    Création vue basée sur la synthese au format SINP spécifique pour le moment plublic

--- data/exports.sql
+++ data/exports.sql
@@ -88,3 +88,103 @@ CREATE VIEW gn_exports.v_exports_logs AS
  ORDER BY start_time;
 
 COMMIT;
+
+
+CREATE OR REPLACE VIEW gn_synthese.v_synthese_sinp AS
+ WITH deco AS (
+         SELECT s_1.id_synthese,
+            n1.label_default AS "ObjGeoTyp",
+            n2.label_default AS "methGrp",
+            n3.label_default AS "obsMeth",
+            n4.label_default AS "obsTech",
+            n5.label_default AS "ocEtatBio",
+            n6.label_default AS "ocStatBio",
+            n7.label_default AS "ocNat",
+            n8.label_default AS "preuveOui",
+            n9.label_default AS "difNivPrec",
+            n10.label_default AS "ocStade",
+            n11.label_default AS "ocSex",
+            n12.label_default AS "objDenbr",
+            n13.label_default AS "denbrTyp",
+            n14.label_default AS "sensiNiv",
+            n15.label_default AS "statObs",
+            n16.label_default AS "dEEFlou",
+            n17.label_default AS "statSource",
+            n18.label_default AS "typInfGeo",
+            n19.label_default AS "ocMethDet"
+           FROM gn_synthese.synthese s_1
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n1 ON s_1.id_nomenclature_geo_object_nature = n1.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n2 ON s_1.id_nomenclature_grp_typ = n2.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n3 ON s_1.id_nomenclature_obs_meth = n3.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n4 ON s_1.id_nomenclature_obs_technique = n4.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n5 ON s_1.id_nomenclature_bio_status = n5.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n6 ON s_1.id_nomenclature_bio_condition = n6.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n7 ON s_1.id_nomenclature_naturalness = n7.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n8 ON s_1.id_nomenclature_exist_proof = n8.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n9 ON s_1.id_nomenclature_diffusion_level = n9.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n10 ON s_1.id_nomenclature_life_stage = n10.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n11 ON s_1.id_nomenclature_sex = n11.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n12 ON s_1.id_nomenclature_obj_count = n12.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n13 ON s_1.id_nomenclature_type_count = n13.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n14 ON s_1.id_nomenclature_sensitivity = n14.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n15 ON s_1.id_nomenclature_observation_status = n15.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n16 ON s_1.id_nomenclature_blurring = n16.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n17 ON s_1.id_nomenclature_source_status = n17.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n18 ON s_1.id_nomenclature_info_geo_type = n18.id_nomenclature
+             LEFT JOIN ref_nomenclatures.t_nomenclatures n19 ON s_1.id_nomenclature_determination_method = n19.id_nomenclature
+        )
+ SELECT s.id_synthese AS "idSynthese",
+    s.unique_id_sinp AS "permId",
+    s.unique_id_sinp_grp AS "permIdGrp",
+    s.count_min AS "denbrMin",
+    s.count_max AS "denbrMax",
+    s.meta_v_taxref AS "vTAXREF",
+    s.sample_number_proof AS "sampleNumb",
+    s.digital_proof AS "preuvNum",
+    s.non_digital_proof AS "preuvNoNum",
+    s.altitude_min AS "altMin",
+    s.altitude_max AS "altMax",
+    s.the_geom_4326 AS geom,
+    s.date_min AS "dateDebut",
+    s.date_max AS "dateFin",
+    s.validator AS validateur,
+    s.observers AS observer,
+    s.id_digitiser,
+    s.determiner AS detminer,
+    s.comment_context AS "obsCtx",
+    s.comment_description AS "obsDescr",
+    s.meta_create_date,
+    s.meta_update_date,
+    d.id_dataset AS "jddId",
+    d.dataset_name AS "jddCode",
+    d.id_acquisition_framework,
+    t.cd_nom AS "cdNom",
+    t.cd_ref AS "cdRef",
+    s.nom_cite AS "nomCite",
+    public.st_x(public.st_transform(s.the_geom_point, 2154)) AS x_centroid,
+    public.st_y(public.st_transform(s.the_geom_point, 2154)) AS y_centroid,
+    COALESCE(s.meta_update_date, s.meta_create_date) AS lastact,
+    deco."ObjGeoTyp",
+    deco."methGrp",
+    deco."obsMeth",
+    deco."obsTech",
+    deco."ocEtatBio",
+    deco."ocNat",
+    deco."preuveOui",
+    deco."difNivPrec",
+    deco."ocStade",
+    deco."ocSex",
+    deco."objDenbr",
+    deco."denbrTyp",
+    deco."sensiNiv",
+    deco."statObs",
+    deco."dEEFlou",
+    deco."statSource",
+    deco."typInfGeo"
+   FROM gn_synthese.synthese s
+     JOIN taxonomie.taxref t ON t.cd_nom = s.cd_nom
+     JOIN gn_meta.t_datasets d ON d.id_dataset = s.id_dataset
+     JOIN gn_synthese.t_sources sources ON sources.id_source = s.id_source
+     JOIN deco ON deco.id_synthese = s.id_synthese;
+
+INSERT INTO gn_exports.t_exports (label, schema_name, view_name, "desc", geometry_field, geometry_srid, public) VALUES ('Synthese SINP', 'gn_synthese', 'v_synthese_sinp', 'Export des données de la synthèse au standard SINP', 'geom', 4326, TRUE);
