a7ab702e458afc37d7431b7e5be03050339b118f
cdcvidal <celine_vidal@natural-solutions.eu>
2019-08-09 18:40:42 +0200

    Nettoyage du code et mise en forme de la liste des exports

--- frontend/app/export-list/export-list.component.ts
+++ frontend/app/export-list/export-list.component.ts
@@ -20,7 +20,7 @@ import { CommonService } from "@geonature_common/service/common.service";
 import { AppConfig } from "@geonature_config/app.config";
 
 import { ModuleConfig } from "../module.config";
-import { Export, ExportService } from "../services/export.service";
+import { Export, ExportService, ApiErrorResponse } from "../services/export.service";
 
 @Component({
   selector: "download-progress-bar",
@@ -63,7 +63,7 @@ export class ProgressComponent {
   providers: []
 })
 export class ExportListComponent implements OnInit {
-  exports$: Observable<Export[]>;
+  exports: Export[];
   public api_endpoint = `${AppConfig.API_ENDPOINT}/${ModuleConfig.MODULE_URL}`;
   public modalForm: FormGroup;
   public buttonDisabled = false;
@@ -74,7 +74,6 @@ export class ExportListComponent implements OnInit {
   private _modalRef: NgbModalRef;
 
 
-  @ViewChild("content") FormatSelector: ElementRef;
   constructor(
     private _exportService: ExportService,
     private _commonService: CommonService,
@@ -92,9 +91,25 @@ export class ExportListComponent implements OnInit {
     });
 
     this.loadingIndicator = true;
-    this._exportService.getExports();
-    this.exports$ = this._exportService.exports;
-    this.loadingIndicator = false;
+
+    this._exportService.getExports()
+      .subscribe(
+        (exports: Export[]) => {
+          this.exports = exports;
+          this.loadingIndicator = false;
+        },
+        (errorMsg: ApiErrorResponse) => {
+          this.toastr.error(
+            errorMsg.error.message ? errorMsg.error.message : errorMsg.message,
+            errorMsg.error.api_error ? errorMsg.error.api_error : errorMsg.name,
+            { timeOut: 0 }
+          );
+          console.error("api error:", errorMsg);
+          this.loadingIndicator = false;
+        },
+      );
+
+
   }
 
   get formatSelection() {
@@ -105,24 +120,17 @@ export class ExportListComponent implements OnInit {
     this._modalRef = this.modalService.open(content);
   }
 
-  selectFormat(id_export: number) {
+  selectFormat(id_export: number, content) {
     this._getOne(id_export);
-    this.open(this.FormatSelector);
+    this.open(content);
   }
 
   _getOne(id_export: number) {
-    this.exports$
-      .switchMap((exports: Export[]) =>
-        exports.filter((x: Export) => x.id == id_export)
-      )
-      .take(1)
-      .subscribe(
-        x => (this._export = x),
-        e => {
-          console.error(e.error);
-          this.toastr.error(e.message, e.name, { timeOut: 0 });
-        }
-      );
+    this._export = this.exports
+    .find((item: Export) => {
+     return item.id == id_export
+    })
+
   }
 
   download() {
@@ -132,6 +140,19 @@ export class ExportListComponent implements OnInit {
       this._exportService.downloadExport(
         this._export,
         this.formatSelection.value
+      ).subscribe(
+        response => {
+          this.toastr.success(
+            response && response.message ? response.message : ''
+          )
+        },
+        (response: ApiErrorResponse) => {
+          this.toastr.error(
+            response.error.message ? response.error.message : response.message,
+            response.error.api_error ? response.error.api_error : response.name,
+            { timeOut: 0 }
+          );
+        }
       );
     }
   }
