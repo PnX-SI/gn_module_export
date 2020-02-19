e49f7224bad8bf7e33aebadd68e9761efc35dd4e
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2020-02-19 08:22:45 +0100

    Mise en forme email Export

--- backend/send_mail.py
+++ backend/send_mail.py
@@ -1,5 +1,5 @@
 """
-     Fonctions permettant l'envoie de mail
+     Fonctions permettant l'envoi d'email
 """
 from flask import url_for, current_app
 
@@ -13,7 +13,7 @@ def export_send_mail(role, export, file_name):
         .. :quickref: Send email after export is done
 
 
-        :query User role: User who run the export
+        :query User role: User who runs the export
         :query {} export: Export definition
         :query str file_name: Name of exported file
     """
@@ -22,15 +22,15 @@ def export_send_mail(role, export, file_name):
     msg = """
         Bonjour,
         <p>
-            Votre export {} est accessible via le lien suivant
-            <a href="{}">Lien de téléchargement</a>
+            Votre export <i>{}</i> est accessible via le lien suivant : 
+            <a href="{}">Téléchargement</a>.
         </p>
         <p>
-            Les données de cet export sont associée à la licence <a href="{}">{}</a>.
-            Merci de les utiliser en respectant la licence.
+            Les données de cet export sont associées à la licence <a href="{}">{}</a>.
+            Merci de les utiliser en respectant cette licence.
         </p>
         <p>
-            <b>Attention : Ce fichier sera supprimé sous {} jours</b>
+            <b>Attention</b> : Ce fichier sera supprimé sous {} jours.
         </p>
     """.format(
         export['label'],
@@ -41,7 +41,7 @@ def export_send_mail(role, export, file_name):
     )
     send_mail(
         recipients=[role.email],
-        subject="[GeoNature]Export {} réalisé".format(export['label']),
+        subject="[GeoNature] Export {} réalisé".format(export['label']),
         msg_html=msg
     )
 
@@ -53,7 +53,7 @@ def export_send_mail_error(role, export, error):
         .. :quickref: Send email after export is failed
 
 
-        :query User role: User who run the export
+        :query User role: User who runs the export
         :query {} export: Export definition
         :query str error: Detail of the exception raised
 
@@ -66,18 +66,18 @@ def export_send_mail_error(role, export, error):
     msg = """
         Bonjour,
         <p>
-            Votre export {} n'a pas fonctionné correctement
+            Votre export <i>{}</i> n'a pas fonctionné correctement.
         </p>
         <p>
             <b>Detail</b>
             {}
         </p>
         <p>
-            Veuillez signaler le problème à l'administrateur du site
+            Veuillez signaler le problème à l'administrateur du site.
         </p>
     """.format(label, error)
     send_mail(
         recipients=[role.email],
-        subject="[GeoNature][ERREUR]Export {}".format(label),
+        subject="[GeoNature][ERREUR] Export {}".format(label),
         msg_html=msg
     )
