0cf9b57f808020016dd221cd91cc211c6f58904f
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2023-05-10 09:36:56 +0200

    style(front): perform prettier on 3 files

--- frontend/app/gnModule.module.ts
+++ frontend/app/gnModule.module.ts
@@ -1,36 +1,32 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { Routes, RouterModule } from "@angular/router";
-import {
-  HttpClientModule,
-  HttpClientXsrfModule,
-  HTTP_INTERCEPTORS
-} from "@angular/common/http";
-import { GN2CommonModule } from "@geonature_common/GN2Common.module";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule, HttpClientXsrfModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { GN2CommonModule } from '@geonature_common/GN2Common.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 // import { NgbModalBackdrop } from "@ng-bootstrap/ng-bootstrap/modal/modal-backdrop";
 
-import { ExportListComponent } from "./export-list/export-list.component";
+import { ExportListComponent } from './export-list/export-list.component';
 
-import { ExportService } from "./services/export.service";
+import { ExportService } from './services/export.service';
 
-const routes: Routes = [{ path: "", component: ExportListComponent }];
+const routes: Routes = [{ path: '', component: ExportListComponent }];
 
 @NgModule({
   imports: [
     HttpClientXsrfModule.withOptions({
-      cookieName: "token",
-      headerName: "token"
+      cookieName: 'token',
+      headerName: 'token',
     }),
     CommonModule,
     GN2CommonModule,
     NgbModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
   ],
   declarations: [ExportListComponent],
   providers: [ExportService],
-  bootstrap: []
+  bootstrap: [],
 })
 export class GeonatureModule {}
