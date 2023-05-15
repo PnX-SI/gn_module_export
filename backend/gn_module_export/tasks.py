dd8a6036285c507365594ada963a62489ba247a9
Pierre Narcisi <pierre.narcisi@mnhn.fr>
2023-05-15 18:14:30 +0200

    Feat(task.py): move funtion clean_export_file to task.py file and remove its call from generateExport

--- backend/gn_module_export/tasks.py
+++ backend/gn_module_export/tasks.py
@@ -1,9 +1,13 @@
+import os
+
 from datetime import datetime, timedelta
+from pathlib import Path
 
 from sqlalchemy import func
 from celery.utils.log import get_task_logger
 from celery.schedules import crontab
 
+from flask import current_app
 from geonature.utils.celery import celery_app
 
 from .models import Export, ExportSchedules
@@ -49,3 +53,36 @@ def generate_export(self, export_id, file_name, export_url, format, id_role, fil
 
     export_data_file(export_id, file_name, export_url, format, id_role, filters)
     logger.info(f"Export {export_id} generated.")
+
+
+def clean_export_file():
+    """
+    Fonction permettant de supprimer les fichiers générés
+    par le module export ayant plus de X jours
+
+    .. :quickref: Fonction permettant de supprimer les
+        fichiers générés par le module export ayant plus de X jours
+
+    """
+    conf = current_app.config.get("EXPORTS")
+
+    dirs_to_del_from = [
+        os.path.join(
+            current_app.config["MEDIA_FOLDER"],
+            conf.get("export_schedules_dir"),
+        ),
+        os.path.join(
+            current_app.config["MEDIA_FOLDER"], conf.get("usr_generated_dirname")
+        ),
+    ]
+    # Date limite de suppression
+    time_to_del = datetime.timestamp(
+        datetime.today()
+        - timedelta(days=current_app.config["EXPORTS"]["nb_days_keep_file"])
+    )
+    for dir in dirs_to_del_from:
+        for item in Path(dir).glob("**", recursive=True):
+            item_time = item.stat().st_mtime
+            if item_time < time_to_del:
+                if item.is_file():
+                    item.unlink()
