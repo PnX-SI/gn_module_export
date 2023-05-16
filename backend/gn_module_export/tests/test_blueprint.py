5539d64d177ba821322628550288a955dff6e91f
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2023-05-16 15:16:49 +0200

    test_get_all_exports_with_scope_0

--- backend/gn_module_export/tests/test_blueprint.py
+++ backend/gn_module_export/tests/test_blueprint.py
@@ -153,6 +153,7 @@ class TestExportsBlueprints:
 
     def test_get_all_exports_with_scope_1_2_group(self, exports, users, group_and_user):
         allowed_fixture_ids = set()
+        allowed_fixture_label = set()
         for _, exp in exports.items():
             if exp.public or exp.label in ("Private1"):
                 allowed_fixture_ids.add(exp.id)
@@ -162,7 +163,20 @@ class TestExportsBlueprints:
         assert response.status_code == 200
         response_id_export = set([exp["id"] for exp in response.json])
         assert allowed_fixture_ids.issubset(response_id_export)
+        response_label_export = set([exp["label"] for exp in response.json])
+        assert allowed_fixture_label.issubset(response_label_export)
 
     def test_get_all_exports_with_scope_0(self, exports, users, group_and_user):
-        # TODO
-        pass
+        # Only public exports OR 403 ????
+        allowed_fixture_ids = set()
+        for _, exp in exports.items():
+            if exp.public:
+                allowed_fixture_ids.add(exp.id)
+
+        set_logged_user_cookie(self.client, users["noright_user"])
+
+        response = self.client.get(url_for("exports.get_exports"))
+        assert response.status_code == 200
+
+        response_id_export = set([exp["id"] for exp in response.json])
+        assert allowed_fixture_ids.issubset(response_id_export)
