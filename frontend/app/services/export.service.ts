698eecc6ea5e91680ee3c96eef1953d643d86e7d
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2023-08-08 11:52:02 +0200

    tool for lint + lint

--- frontend/app/services/export.service.ts
+++ frontend/app/services/export.service.ts
@@ -22,7 +22,10 @@ export interface ApiErrorResponse extends HttpErrorResponse {
 
 @Injectable()
 export class ExportService {
-  constructor(private _api: HttpClient, public config: ConfigService) {}
+  constructor(
+    private _api: HttpClient,
+    public config: ConfigService,
+  ) {}
 
   getExports() {
     console.log(this.config.API_ENDPOINT, this.config.EXPORTS.MODULE_URL);
@@ -32,7 +35,7 @@ export class ExportService {
   downloadExport(x: Export, format: string) {
     return this._api.post<any>(
       `${this.config.API_ENDPOINT}${this.config.EXPORTS.MODULE_URL}/${x.id}/${format}`,
-      {}
+      {},
     );
   }
 }
