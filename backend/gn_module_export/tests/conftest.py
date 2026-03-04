4de74f7426ef43494b177f0d6c940f2e92035f8f
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2026-03-04 19:16:47 +0100

    fix backend test

--- backend/gn_module_export/tests/conftest.py
+++ backend/gn_module_export/tests/conftest.py
@@ -1,3 +1,4 @@
 from geonature.tests.fixtures import *
 from geonature.tests.fixtures import _session, app, _app, users
 from geonature.tests.test_permissions import g_permissions
+from gn_module_export.tests.fixtures import *
